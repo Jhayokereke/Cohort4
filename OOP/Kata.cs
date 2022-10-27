ffdfc035acfa89f9dc38e4c4a3bf9c16c7ae431a
Chukwuemeka Okereke <cokereke@axxess.com>
2022-10-27 15:56:30 +0100

    Algo

---
+++ OOP/Kata.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP
+{
+    public static class Kata
+    {
+        public static int СenturyFromYear(int year)
+        {
+            if (year % 100 > 0)
+            {
+                return (year / 100) + 1;
+            }
+            else
+            {
+                return year / 100;
+            }
+
+            //return (year + 99) / 100;
+
+            //return year % 100 > 0 ? (year / 100) + 1 : year / 100;
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,12 +6,23 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            MathematicsOperations mathOp = new MathematicsOperations();
-
-            int result = mathOp.Add(4, 5, new int[] {2, 3, 4, 6, 11});
-
-            Console.WriteLine(result);
-
+            int numerator = 1800;
+            int divisor = 100;
+            int quotient = numerator / divisor;
+            int remainder = numerator % divisor;
+            Console.WriteLine($"{numerator} divided by {divisor} gives us {quotient} remainder {remainder}");
+            
+            //1234 + 99 = 1333 / 100 = 13;
+            //1800 + 99 = 1899 / 100 = 18;
+            //3467 + 99 = 3566 / 100 = 35;
+            int[] years = new int[] { 1234, 1800, 3467, 235, 1888, 1960, 2020, 2021, 2037, 1405, 1914, 1287 };
+            for (int i = 0; i < years.Length; i++)
+            {
+                Console.WriteLine();
+                int year = years[i];
+                int century = Kata.СenturyFromYear(year);
+                Console.WriteLine($"The year {year} falls into the {century} century.");
+            }
         }
     }
 }
