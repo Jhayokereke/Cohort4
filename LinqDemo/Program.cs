1d70b137dd9b1976a7ced62977e1013e899b965a
Chukwuemeka Okereke <cokereke@axxess.com>
2022-12-14 11:34:35 +0100

    break

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -3,7 +3,10 @@
  * 
  * Projection - Return a collection/group/item in a different format
  * Select - Return a single collection/item based in a new format
- * SelectMany - 
+ * SelectMany - Returns a flattened out colection/item in a new format
+ * 
+ * Filteration - pick certain items from a larger collection/group
+ * Where - returns a new collection that matches a given criteria/condition
  */
 
 List<string> names = new()
@@ -18,4 +21,21 @@ var query = from x in names
 
 var groupOfNames = names.SelectMany(x => x);
 
-Console.WriteLine(string.Join("\n", groupOfNames));
+Dictionary<string, List<string>> sportRecord = new()
+{
+    { "high jump", new List<string> { "Ade", "Bolu", "Sola" } },
+    { "long jump", new List<string> { "Greg", "Sina", "Tega" } },
+    { "Pole vault", new List<string> { "Kate", "Pam", "Irene" } },
+    { "Tennis", new List<string> { "Paul", "Saul", "John" } },
+    { "Discus", new List<string> { "Messi", "Ronaldo", "Mbappe" } }
+};
+
+var allWinners = sportRecord.SelectMany(sr => sr.Value);
+
+var fiveLetterNames = names.Where(name =>  name.Length == 5);
+var fiveLettersOrMore = names.Where(name => name.Length >= 5);
+
+var startsWithA = names.Where(n => n[0] == 'A');
+var startsWithR = names.Where(n => n[0] == 'R');
+
+Console.WriteLine(string.Join("\n", fiveLetterNames));
